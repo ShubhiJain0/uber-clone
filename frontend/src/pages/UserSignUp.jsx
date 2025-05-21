a0e6f2dd5b7dc00a0111096760d6222f43d42886
Shubhi Jain <jainshubhi328@gmail.com>
2025-05-21 14:38:33 +0530

    Update UserSignUp.jsx

--- frontend/src/pages/UserSignUp.jsx
+++ frontend/src/pages/UserSignUp.jsx
@@ -44,7 +44,7 @@ const UserSignUp = () => {
 
     
 
-    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser , { withCredentials: true })
 
     if(response.status ===201){
       const data = response.data;
