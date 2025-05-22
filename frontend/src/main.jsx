17d2086eaa1197e10dd330b3b1ecb7560391e2bc
ShubhiJain0 <jainshubhi328@gmail.com>
2025-05-22 20:20:38 +0530

    latestt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,17 @@ import UserContext from './context/UserContext.jsx'
 import SocketContext from './context/SocketContext.jsx'
 import CaptainContext from './context/CaptainContext.jsx'
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CaptainContext>
